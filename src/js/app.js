2fb8580667f318c05a5b08911037e40681a51e9c
ateranCode <adrianjoop@gmail.com>
2026-04-15 10:18:12 -0400

    Agregando proyecto

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const btnMenu = document.querySelector(".mobile-menu");
+  const navegacion = document.querySelector(".navegacion-principal");
+
+  btnMenu.addEventListener("click", () => {
+    btnMenu.classList.toggle("active");
+    navegacion.classList.toggle("active");
+  });
+
+  // Cerrar al hacer click en un enlace (importante en One Page)
+  const enlaces = document.querySelectorAll(".navegacion-principal a");
+  enlaces.forEach((link) => {
+    link.addEventListener("click", () => {
+      btnMenu.classList.remove("active");
+      navegacion.classList.remove("active");
+    });
+  });
+});
